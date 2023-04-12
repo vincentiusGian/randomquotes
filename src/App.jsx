4a8e1f4ae53ccd2b8d02ecc8b4aa893cf814278e
Gian Junius <gianjunius54@gmail.com>
2023-04-12 21:23:19 +0700

    akj

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,15 @@ function App() {
   }
 
   return (
-    <div className="App min-h-screen dark:text-white dark:bg-slate-700 block grid justify-items-center">
+    <div className="App min-h-screen text-black dark:text-white dark:bg-slate-700 block grid justify-items-center">
       {/* <div className="w-64 carousel rounded-box">
         <div className="carousel-item w-full">tes</div>
       </div> */}
-      <h1 className="text-5xl font-bold text-white">HAI CANTIK!</h1>
-      <p className="m-10 mt-0">in case you don't in a good mood today, just remember this websites okayy? &lt;3</p>
-      <div className="card w-96 bg-sky-950 shadow-xl min-h-fit">
+      <div className="block justify-items-center">
+      <h1 className="text-5xl font-bold">HAI CANTIK!</h1>
+      <p className="m-auto">in case you don't in a good mood today, just remember this websites okayy? &lt;3</p>
+      </div>
+      <div className="card w-96 bg-sky-950 text-white shadow-xl m-10">
         <div className="card-body">    
       <h2 className="card-title">Inspirational Quotes!!!</h2>
       <p className="text-lg">{quotes ? <><p>"{quotes.text}" - {quotes.author}.</p> <br /> <p className="text-sm">Have a good day Via! I know sometimes it'll be hard, but I'm here to keep you safe, okayy? I love you!</p></> : <p>Click the button please &lt;3</p>}</p>
